2002148de76ebb1d5b56b250be66f5a8cc3b98c4
hongphuc <hongphuc.dthp@gmail.com>
2019-09-04 15:03:33 +0700

    Add index template successful

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module MockProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.assets.paths << Rails.root.join('app', 'assets', 'images')
+
+    Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+      config.assets.paths << path
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
