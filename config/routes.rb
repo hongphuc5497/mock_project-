90924532c278b99fd7ab69cb7fb7bc69af43662e
hongphuc <hongphuc.dthp@gmail.com>
2019-09-05 16:51:25 +0700

    Fix errors routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'top#index', as: 'top'
 
+  #middleware for pages
+  match "/404", to: "errors#not_found", via: :all
+  match "/422", to: "errors#unprocessable", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all 
+
   resources :blog, only: %i[index show]
 end
