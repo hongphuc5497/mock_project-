89836b26f9447b4a79c485c5d3a979f6978b4b7b
hongphuc <hongphuc.dthp@gmail.com>
2019-09-06 12:05:12 +0700

    Use gem breadcrumb

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,7 +1,16 @@
 class BlogController < ApplicationController
+  impressionist 
+
   def index
+    add_breadcrumb 'Blog', :blog_index_path 
+
+    @blogs = Blog.all
   end
 
   def show
+    @blog = Blog.find(params[:id])
+
+    add_breadcrumb 'Blog',                  :blog_show_path
+    add_breadcrumb @blog.blog_detail.title, :blog_path 
   end
 end
